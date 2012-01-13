19928929a2cb71b99f319f0679cfe495b0fcf0b6
Justin Phelps <linuturk@onitato.com>
2012-01-12 22:07:45 -0600

    whoops.

--- index.php
+++ index.php
@@ -6,9 +6,9 @@ $title="Lunch Schedule";
 $times = array(
 	1100 => "11:00 AM",
 	1200 => "12:00 PM",
-	1300 => "&nbsp;1:00 PM",
-	1400 => "&nbsp;2:00 PM",
-	1500 => "&nbsp;3:00 PM"
+	1300 => "1:00 PM",
+	1400 => "2:00 PM",
+	1500 => "3:00 PM"
 );
 
 html_header($title);
