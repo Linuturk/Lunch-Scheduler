722b29af1989eb74b11f76dc62c95f0e858b26ba
Justin Phelps <linuturk@onitato.com>
2012-01-13 01:58:29 -0600

    Hope this works.

--- index.php
+++ index.php
@@ -4,6 +4,7 @@ require_once("functions.inc");
 
 $title="Lunch Schedule";
 $schedule_file="/tmp/temp.txt";
+$schedule_array = read_file($schedule_file);
 $times = array(
 	1100 => "11:00 AM",
 	1200 => "12:00 PM",
@@ -13,17 +14,33 @@ $times = array(
 );
 
 html_header($title);
+
 echo "<h1>lunch.onitato.com</h1>\n";
 
-reset_file($schedule_file);
-append_file($schedule_file, "John", "slot0");
-append_file($schedule_file, "Jane", "slot1");
-append_file($schedule_file, "Sam", "slot2");
+if (!empty($_POST)) {
+	$action = $_POST["submit"];
+	if ($action == "Add") {
+		$name = $_POST["name"];
+		$time = $_POST["timeslot"];
+		echo "<p>Adding $name to the schedule at $time.</p>\n";
+		append_file($schedule_file, $name, $time);
+		display_form($times);
+	} elseif ($action == "Clear") {
+		echo "<p>Clearing the schedule.</p>\n";
+		reset_file($schedule_file);
+		display_form($times);
+	} elseif ($action == "View") {
+		echo "<p>Here is the schedule.</p>\n";
+		display_schedule($schedule_array);
+		display_form($times);
+	} else {
+		echo "<h1>HOW DID I GET HERE?!</h1>\n";
+	}
+} else {
+	display_schedule($schedule_array);
+	display_form($times);
+}
 
-$schedule = read_file($schedule_file);
-display_schedule($schedule);
-
-//form_logic($times);
 html_footer();
 
 ?>
