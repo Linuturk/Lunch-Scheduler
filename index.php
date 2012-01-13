b89881b3d46309d2c45c1bc9e23c055e4547f697
Justin Phelps <linuturk@onitato.com>
2012-01-12 23:08:39 -0600

    Echo statements.

--- index.php
+++ index.php
@@ -15,10 +15,15 @@ $times = array(
 html_header($title);
 echo "<h1>lunch.onitato.com</h1>\n";
 
+echo "Trying to reset file.\n";
 reset_file($schedule_file);
+echo "Trying to append to file.\n";
 append_file($schedule_file, "John", "slot0");
+echo "Trying to append to file.\n";
 append_file($schedule_file, "Jane", "slot1");
+echo "Trying to append to file.\n";
 append_file($schedule_file, "Sam", "slot2");
+echo "Trying to read the file.\n";
 read_file($schedule_file);
 
 //form_logic($times);
