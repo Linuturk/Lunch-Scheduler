12189b308c2f4a63aec08bffa53bb66de2886a6e
Justin Phelps <linuturk@onitato.com>
2012-01-12 20:39:25 -0600

    More code to functions.

--- index.php
+++ index.php
@@ -3,9 +3,16 @@
 require_once("functions.inc");
 
 $title="Lunch Schedule";
+$times = array(
+	1100 => "11:00 AM",
+	1200 => "12:00 PM",
+	1300 => "1:00 PM",
+	1400 => "2:00 PM",
+	1500 => "3:00 PM"
+);
 
 html_header($title);
-
+display_schedule($times);
 html_footer();
 
 ?>
