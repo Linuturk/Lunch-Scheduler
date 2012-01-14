09dcdf78cb919d497975e7b67d9c9ed370f8df5a
Justin Phelps <linuturk@onitato.com>
2012-01-13 19:48:53 -0600

    Bunch of changes. Switching to a database. Flat files suck.

--- index.php
+++ index.php
@@ -2,9 +2,14 @@
 
 require_once("functions.inc");
 
+$db_host = "localhost";
+$db_user = "lunch";
+$db_pass = "lunchpassword";
+$db_database = "lunch";
+
+database($db_host, $db_database, $db_user, $db_pass);
+
 $title="Lunch Schedule";
-$schedule_file="/tmp/temp.txt";
-$schedule_array = read_file($schedule_file);
 $times = array(
 	1100 => "11:00 AM",
 	1200 => "12:00 PM",
@@ -23,22 +28,15 @@ if (!empty($_POST)) {
 		$name = $_POST["name"];
 		$time = $_POST["time"];
 		echo "<p>Adding $name to the schedule at $time.</p>\n";
-		append_file($schedule_file, $name, $time);
-		display_form($times);
 	} elseif ($action == "Clear") {
 		echo "<p>Clearing the schedule.</p>\n";
-		reset_file($schedule_file);
-		display_form($times);
 	} elseif ($action == "View") {
 		echo "<p>Here is the schedule.</p>\n";
-		display_schedule($schedule_array);
-		display_form($times);
 	} else {
 		echo "<h1>HOW DID I GET HERE?!</h1>\n";
 	}
 } else {
-	display_schedule($schedule_array);
-	display_form($times);
+	echo "Let's get started.";
 }
 
 html_footer();
